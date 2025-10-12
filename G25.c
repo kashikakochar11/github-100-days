b7b8e891073a31104a1cdeb413e41e48998b40ab
kashikakochar11 <kashikakochar11@gmail.com>
2025-10-12 23:59:42 +0530

    Add files via upload

---
+++ G25.c
@@ -0,0 +1,56 @@
+//Write a program to implement a basic calculator using switch-case for +, -, *, /, %.
+#include <stdio.h>
+int main()
+{
+    char operator;
+    float num1, num2, result;
+
+    // Input operator and two numbers
+    printf("Enter an operator (+, -, *, /, %%): ");
+    scanf(" %c", &operator);
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+    // Perform calculation based on the operator
+    switch (operator)
+    {
+        case '+':
+            result = num1 + num2;
+            printf("%.2f + %.2f = %.2f\n", num1, num2, result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("%.2f - %.2f = %.2f\n", num1, num2, result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("%.2f * %.2f = %.2f\n", num1, num2, result);
+            break;
+        case '/':
+            if (num2 != 0)
+            {
+                result = num1 / num2;
+                printf("%.2f / %.2f = %.2f\n", num1, num2, result);
+            }
+            else
+            {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+        case '%':
+            if ((int)num2 != 0)
+            {
+                result = (int)num1 % (int)num2;
+                printf("%d %% %d = %d\n", (int)num1, (int)num2, (int)result);
+            }
+            else
+            {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+        default:
+            printf("Error: Invalid operator.\n");
+    }
+
+    return 0;
+}

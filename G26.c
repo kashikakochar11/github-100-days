c7ebdfb03a548291785e0075be8f338734050e78
kashikakochar11 <kashikakochar11@gmail.com>
2025-10-13 23:54:21 +0530

    Add files via upload
    Write a program to print numbers from 1 to n.

---
+++ G26.c
@@ -0,0 +1,13 @@
+//Write a program to print numbers from 1 to n without array
+#include <stdio.h>
+int main()
+{
+    int n,i;
+    printf("Enter the value of n\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+}

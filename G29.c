166cae073ac1f8490adb579c776fbcb1051dcf0b
kashikakochar11 <kashikakochar11@gmail.com>
2025-10-14 23:52:03 +0530

    Add files via upload
    WAP to print factorial of a number.

---
+++ G29.c
@@ -0,0 +1,15 @@
+//WAP to print factorial of a number.
+#include <stdio.h>
+int main()
+{
+    int n,i,f=1;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        f=f*i;
+    }
+    printf("Factorial of %d is %d",n,f);
+    return 0;
+}
